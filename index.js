8995918bab30c7bc49e2607b682f85fe7f9ea622
novan921 <38156573+novan921@users.noreply.github.com>
2020-09-25 23:54:06 +0700

    return bot functionality, added more dependencies

--- index.js
+++ index.js
@@ -1,132 +1,91 @@
-const config = require("./config.json"),
-Discord = require("discord.js"),
-Parser = require("rss-parser"),
-parser = new Parser(),
-Youtube = require("simple-youtube-api"),
-youtube = new Youtube(config.youtubeKey);
+const fs = require('fs');
 
-//"message": "New video **{videoTitle}** by **{videoAuthorName}** uploaded at **{videoPubDate}**! Link: {videoURL}",
+// require the discord.js module
+const Discord = require('discord.js');
 
+// config file
+const { prefix } = require('./config.json');
 
-const startAt = Date.now();
-const lastVideos = {};
-
+// create a new Discord client
 const client = new Discord.Client();
-client.login(process.env.TOKEN).catch(console.log);
 
-client.on("ready", () => {
-    console.log(`[!] Ready to listen ${config.youtubers.length} youtubers!`);
-    check();
-    setInterval(check, 20*1000);
+// get all .js file in folder commands
+// and assign them to the collection
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// create  cooldowns
+const cooldowns = new Discord.Collection();
+
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+    console.log('Ready!');
 });
 
-/**
- * Format a date to a readable string
- * @param {Date} date The date to format 
- */
-function formatDate(date) {
-    let monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-    let day = date.getDate(), month = date.getMonth(), year = date.getFullYear();
-    return `${day} ${monthNames[parseInt(month, 10)]} ${year}`;
-}
+client.on('message', message => {
 
-/**
- * Call a rss url to get the last video of a youtuber
- * @param {string} youtubeChannelName The name of the youtube channel
- * @param {string} rssURL The rss url to call to get the videos of the youtuber
- * @returns The last video of the youtuber
- */
-async function getLastVideo(youtubeChannelName, rssURL){
-    console.log(`[${youtubeChannelName}]  | Getting videos...`);
-    let content = await parser.parseURL(rssURL);
-    console.log(`[${youtubeChannelName}]  | ${content.items.length} videos found`);
-    let tLastVideos = content.items.sort((a, b) => {
-        let aPubDate = new Date(a.pubDate || 0).getTime();
-        let bPubDate = new Date(b.pubDate || 0).getTime();
-        return bPubDate - aPubDate;
-    });
-    console.log(`[${youtubeChannelName}]  | The last video is "${tLastVideos[0] ? tLastVideos[0].title : "err"}"`);
-    return tLastVideos[0];
-}
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-/**
- * Check if there is a new video from the youtube channel
- * @param {string} youtubeChannelName The name of the youtube channel to check
- * @param {string} rssURL The rss url to call to get the videos of the youtuber
- * @returns The video || null
- */
-async function checkVideos(youtubeChannelName, rssURL){
-    console.log(`[${youtubeChannelName}] | Get the last video..`);
-    let lastVideo = await getLastVideo(youtubeChannelName, rssURL);
-    // If there isn't any video in the youtube channel, return
-    if(!lastVideo) return console.log("[ERR] | No video found for "+lastVideo);
-    // If the date of the last uploaded video is older than the date of the bot starts, return 
-    if(new Date(lastVideo.pubDate).getTime <= startAt) return console.log(`[${youtubeChannelName}] | Last video was uploaded before the bot starts`);
-    let lastSavedVideo = lastVideos[youtubeChannelName];
-    // If the last video is the same as the last saved, return
-    if(lastSavedVideo && (lastSavedVideo.id === lastVideo.id)) return console.log(`[${youtubeChannelName}] | Last video is the same as the last saved`);
-    return lastVideo;
-}
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-/**
- * Get the youtube channel id from an url
- * @param {string} url The URL of the youtube channel
- * @returns The channel ID || null
- */
-function getYoutubeChannelIdFromURL(url) {
-    let id = null;
-    url = url.replace(/(>|<)/gi, "").split(/(\/channel\/|\/user\/)/);
-    if(url[2]) {
-      id = url[2].split(/[^0-9a-z_-]/i)[0];
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+    if (!command) return;
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
     }
-    return id;
-}
 
-/**
- * Get infos for a youtube channel
- * @param {string} name The name of the youtube channel or an url
- * @returns The channel info || null
- */
-async function getYoutubeChannelInfos(name){
-    console.log(`[${name.length >= 10 ? name.slice(0, 10)+"..." : name}] | Resolving channel infos...`);
-    let channel = null;
-    /* Try to search by ID */
-    let id = getYoutubeChannelIdFromURL(name);
-    if(id){
-        channel = await youtube.getChannelByID(id);
+    if (command.args && !args.length) {
+
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+
     }
-    if(!channel){
-        /* Try to search by name */
-        let channels = await youtube.searchChannels(name);
-        if(channels.length > 0){
-            channel = channels[0];
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
         }
     }
-    console.log(`[${name.length >= 10 ? name.slice(0, 10)+"..." : name}] | Title of the resolved channel: ${channel.raw ? channel.raw.snippet.title : "err"}`);
-    return channel;
-}
 
-/**
- * Check for new videos
- */
-async function check(){
-    console.log("Checking...");
-    config.youtubers.forEach(async (youtuber) => {
-        console.log(`[${youtuber.length >= 10 ? youtuber.slice(0, 10)+"..." : youtuber}] | Start checking...`);
-        let channelInfos = await getYoutubeChannelInfos(youtuber);
-        if(!channelInfos) return console.log("[ERR] | Invalid youtuber provided: "+youtuber);
-        let video = await checkVideos(channelInfos.raw.snippet.title, "https://www.youtube.com/feeds/videos.xml?channel_id="+channelInfos.id);
-        if(!video) return console.log(`[${channelInfos.raw.snippet.title}] | No notification`);
-        let channel = client.channels.cache.get(config.channel);
-        if(!channel) return console.log("[ERR] | Channel not found");
-        channel.send(
-            config.message
-            .replace("{videoURL}", video.link)
-            .replace("{videoAuthorName}", video.author)
-            .replace("{videoTitle}", video.title)
-            .replace("{videoPubDate}", formatDate(new Date(video.pubDate)))
-        );
-        console.log("Notification sent !");
-        lastVideos[channelInfos.raw.snippet.title] = video;
-    });
-}
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+
+});
+
+// login to Discord with your app's token
+client.login(process.env.TOKEN);
---
+++ run.js
@@ -0,0 +1,91 @@
+const cheerio = require('cheerio');
+const E = require('events');
+const request = require('request');
+const separateReqPool = { maxSockets: 15 };
+const async = require('async');
+const _ = require('lodash');
+let tweets = {}, apiurls = [], N = [];
+
+
+///////////////////////////  CONFIGURE TWITTER HANDLERS /////////////////////////////////////////////////////
+var THandlers = [
+    {
+        name: 'Watson Amelia',
+        url: "https://twitter.com/AchmadSetyabudi",
+        webhook: "https://discordapp.com/api/webhooks/728746704895213628/QJcnGMUAl0kMY8_gEuFmv2QnyRxfv6cu6cQUsFEk5UAjGyn3tGbrHnJTy08a1feFdILX",
+        avatar_url: "https://pbs.twimg.com/profile_images/1303521201331998720/IT-LaNr3_400x400.jpg",
+        keywords: "#test",
+    }
+];
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ADD TWEETS
+THandlers.forEach((th, i) => {
+    tweets[th.url] = [];
+    apiurls.push(th.url);
+});
+
+//DISCORD WEBHOOK
+const sendDiscordMessage = (pl) => {
+    const { content, turl } = pl;
+    const { name, webhook, avatar_url } = THandlers.filter((d, i) => d.url === turl)[0];
+    request.post(webhook).form({ username: name, avatar_url: avatar_url, content: content });
+}
+
+console.log('Twitter => Discord program is running');
+
+const headers = { 'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36' }
+
+//MONITOR
+setInterval(() => {
+    async.map(apiurls, function (item, callback) {
+        request({ url: item, headers, pool: separateReqPool }, function (error, response, body) {
+            try {
+                const $ = cheerio.load(body);
+                var turl = "https://twitter.com" + response.req.path;
+                const th_name = THandlers.filter((d, i) => d.url === turl)[0].name;
+                if (!tweets[turl].length) {
+                    //FIRST LOAD
+                    (() => {
+                        const turls = $('table.tweet').toArray().map(d => d["attribs"]["href"]);
+                        const tweethtml = $('div.tweet-text').toArray().map(d => $(d).html());
+                        tweets[turl] = turls.map((d, i) => { return { url: turls[i], text: tweethtml[i] } });
+                    })()
+                }
+                else {
+                    (() => {
+                        //EVERY OTHER TIME
+                        const turls = $('table.tweet').toArray().map(d => d["attribs"]["href"]);
+                        const tweethtml = $('div.tweet-text').toArray().map(d => $(d).html());
+
+                        const newTweets = turls.map((d, i) => { return { url: turls[i], text: tweethtml[i] } });
+
+                        const oldturls = tweets[turl].map(d => d.url);
+                        const slicelen = turls.map(d => { return oldturls.indexOf(d); }).indexOf(0);
+
+                        const ntweets = slicelen === -1 ? [] : newTweets.slice(0, slicelen);
+
+                        ntweets.filter(d => {
+                            const th_kw = THandlers.filter((d, i) => d.url === turl)[0].keywords.split(',');
+                            if (th_kw.includes('*')) {
+                                return true;
+                            } else {
+                                const checkTexts = th_kw.map(kw => d.text.includes(kw) ? true : false);
+                                return checkTexts.includes(false) ? false : true;
+                            }
+                        }).forEach(t => {
+                            const { url: content } = t;
+                            sendDiscordMessage({ content: `https://twitter.com${content}`, turl });
+                        });
+                        tweets[turl] = newTweets;
+                    })()
+                }
+
+            } catch (e) {
+                console.log('Error =>' + e);
+            }
+        });
+    }, function (err, results) {
+        //console.log(results);
+    });
+}, 1*1000);//RUNS EVERY 1 SECONDS
